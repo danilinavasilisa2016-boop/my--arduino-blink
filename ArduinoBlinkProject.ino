c9f520f89bc81645a38f7de979a5d306f3b74c2b
vasilisa <danilina.vasilisa2016@gmail.com>
2026-02-16 12:26:47 +0300

    feat: add second LED on pin 12, alternate blinking

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -25,13 +25,14 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(500);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(500);                      // wait for a second
+  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(100);                      // wait for a second
+  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
+  delay(100);                      // wait for a second
 }

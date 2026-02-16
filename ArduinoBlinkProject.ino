3eddce8fd6521260332a4b3a11f69692eaeb2feb
vasilisa <danilina.vasilisa2016@gmail.com>
2026-02-16 12:38:56 +0300

    feat: add blinkSOS function

--- ArduinoBlinkProject.ino
+++ ArduinoBlinkProject.ino
@@ -1,38 +1,49 @@
-/*
-  Blink
-
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://docs.arduino.cc/hardware/
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://docs.arduino.cc/built-in-examples/basics/Blink/
-*/
-
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(12, OUTPUT);
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(12, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(13, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+  
+  digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  delay(1000);
+  
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000);
+  
+  
+  blinkSOS();
+}
+
+// Функция для подачи сигнала SOS
+void blinkSOS() {
+  // буква S (три коротких сигнала)
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
+  
+  delay(500);
+  //буква о
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(600);
+    digitalWrite(13, LOW);
+    delay(600);
+  }
+  
+  delay(500);  
+ 
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(200);
+    digitalWrite(13, LOW);
+    delay(200);
+  }
+  
+  delay(2000);  
 }
